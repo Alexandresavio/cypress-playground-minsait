bfb3553cee6ccf9bf49085c3a91cffd759e4e0d3
savio <alexandresaviosouza@gmail.com>
2025-07-25 15:57:42 -0300

    praticando comandos de input com cypress

--- cypress/e2e/teste.cy.js
+++ cypress/e2e/teste.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Teste - Formulario', () => {
+  it('Acessar aplicação e preencher campos do formulário com sucesso', () => {
+    cy.visit('https://testautomationpractice.blogspot.com/');
+    cy.get('#name').type('Savio de Souza');
+    cy.get('input[placeholder="Enter EMail"]').type('teste@gmail.com');
+    cy.get('#phone').type('5199926458');
+    cy.get('#textarea').type('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry')
+    
   })
+
 })
